71b1872baf56520b57bc3768291d0b6d0499074d
hira22 <m.hiraoka22@gmail.com>
2020-08-14 06:41:30 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Masayuki Hiraoka on 2020/08/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SwiftUI2GridSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2GridSampleApp.swift
+//  Shared
+//
+//  Created by Masayuki Hiraoka on 2020/08/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2GridSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
